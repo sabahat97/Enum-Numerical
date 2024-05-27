2759df0ff6fe61ec6e75d711827e9099ca82d2a1
syeda <sbtshakeel61@gmail.com>
2024-05-28 00:26:29 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+//numericals enum
+
+enum Days {
+    Monday=5,  //5 6 7  {index no , we initial from 5 not 0}
+    Tuesday,  //6 its index no
+    Wednessday //7 index no
+}
+
+
+console.log (Days.Wednessday); //output 7 index no
+
+//if we print 
+console.log(Days[7]) //output  wednessday property;
+
+//and if we print
+console.log (Days.Monday) //output assign value 5 
+
+//enum and their properties must be capital like Monday... , Days:
